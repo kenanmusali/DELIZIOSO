39c3172aeb8358bf4ad61b44128d6a49179beaab
kenanmusali <textme0306@gmail.com>
2024-02-29 13:02:32 -0800

    0.1

--- Assets/main.js
+++ Assets/main.js
@@ -1,128 +1,39 @@
-//Menu right
+const orderNowButtons = document.querySelectorAll('.OrderId');
 
-const cart =  document.querySelector('.cart');
-const closecartBtn = document.querySelector('.closecart');
-const contenedorCarrito =  document.querySelector('#lista-carrito tbody');
-const carrito = document.querySelector('#minicart');
-const vaciarCarritoBtn =  document.querySelector('#vaciar-carrito');
-
-//Body
-const courseList = document.querySelector('.cards');
-let articulosCarrito = [];
-let total = 0;
-
-function loadEventListeners(){
-    //Menu right cart
-    // bag.addEventListener('click', openCart);
-
-    //Close Cart button
-    // closecartBtn.addEventListener('click', closecart)
-
-    //Agregar curso presionando addToCart
-    courseList.addEventListener('click', addCourse);
-
-    //Eliminar articulo
-    carrito.addEventListener('click', eliminarCurso);
-
-    //Vaciar carrito
-    vaciarCarritoBtn.addEventListener('click', ()=>{
-        articulosCarrito = []; // reseteamos carrito
-        limpiarHTML()
-    })
-}
-
-loadEventListeners()
-//Open cart
-function openCart(e){
-    e.preventDefault();
-    cart.classList.add('activo')
-}
-//Close Cart
-function closecart(e) {
-    e.preventDefault();
-    cart.classList.remove('activo')
-}
-
-//Btn add course
-function addCourse(e) {
-    e.preventDefault();
-    if (e.target.classList.contains('button')) {
-        const cursoSelected = e.target.parentElement;
-        datosCurso(cursoSelected)
-    }
-}
-
-//Delete product
-function eliminarCurso(e){
-    if(e.target.classList.contains('deletebtn')) {
-        const cursoID = e.target.getAttribute('data-id');
-        articulosCarrito = articulosCarrito.filter( curso => curso.id !== cursoID);
-
-        carritoHTML();
-    }
-}
-
-//Read course data
-function datosCurso(curso){
-
-    //Create Object
-    const infoCurso = {
-        image: curso.querySelector('img').src,
-        title: curso.querySelector('.title3').textContent,
-        price: curso.querySelector('.text3').textContent,
-        id: curso.querySelector('button').getAttribute('data-id'),
-        cantidad: 1
-    }
-
-
-    const existe = articulosCarrito.some( curso => curso.id === infoCurso.id);
-    if(existe){
-        let cursos = articulosCarrito.map(curso =>{
-            if(curso.id === infoCurso.id){
-                curso.cantidad++;
-                return curso;
-            }else {
-                return curso;
-            }
-        });
-        articulosCarrito = [...cursos];
-    }else{
-        //Agrega elementos al array carrito
-        articulosCarrito = [...articulosCarrito, infoCurso];
-    }
-    //console.log(articulosCarrito);
-    carritoHTML();
-}
-
-
-//Muestra carrito en el HTML
-function carritoHTML() {    
-    //Limpiar HTML
-    limpiarHTML();
-    articulosCarrito.forEach( curso =>{
+orderNowButtons.forEach(button => {
+    button.addEventListener('click', function(e) {
         
+        e.preventDefault();
+        const product = this.closest('.divMenuProduct'); 
+
         const row = document.createElement('tr');
         row.innerHTML = `
-            <td><img src="${curso.image}" width="100"></td>
-            <td>${curso.title}</td>
-            <td>${curso.price}</td>
-            <td>${curso.cantidad}</td>
-            <td><a href="#" class="deletebtn" data-id="${curso.id}">X</a></td>
+            <td><img src="${product.querySelector('.img2').src}" width="100"></td>
+            <td>${product.querySelector('.title3').textContent}</td>
+            <td>${product.querySelector('.text3').textContent}</td>
         `;
-        //Agregar en el Tbody
-        contenedorCarrito.appendChild(row);
 
-        //Total
-        total = curso.cantidad + curso.cantidad
-        console.log(total);
-    })
-    
-}
+        document.querySelector('tbody').appendChild(row);
+    });
+});
 
-function limpiarHTML(){
-    // contenedorCarrito.innerHTML = '';
 
-    while(contenedorCarrito.firstChild){
-        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
-    }
-}
+// const orderNowButtons = document.querySelectorAll('.OrderId');
+
+// orderNowButtons.forEach(button => {
+//     button.addEventListener('click', function(e) {
+
+//         if (e.target.tagName === 'BUTTON') {
+//             const product = this.closest('.divMenuProduct');
+
+//             const row = document.createElement('tr');
+//             row.innerHTML = `
+//                 <td><img src="${product.querySelector('.img2').src}" width="100"></td>
+//                 <td>${product.querySelector('.title3').textContent}</td>
+//                 <td>${product.querySelector('.text3').textContent}</td>
+//             `;
+
+//             document.querySelector('tbody').appendChild(row);
+//         }
+//     });
+// });
